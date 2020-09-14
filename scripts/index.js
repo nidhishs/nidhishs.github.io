6734fea01f8ff0d48c851ed6d5b6fce09e3ae15b
Nidhish <55269918+nidhishs@users.noreply.github.com>
2020-09-14 14:34:36 +0200

    Landing page v1.0
    Added HELLO text and interactive blob-scene.

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+gsap.registerPlugin(MorphSVGPlugin);
+gsap.registerPlugin(DrawSVGPlugin);
+
+let prevBlob = 0;
+function changeBlob() {
+    const min = 0, max = 5;
+    let nos = Math.floor(Math.random() * (max - min + 1) + min);
+
+    //Prevent duplicate blobs on success event fires.
+    if (nos == prevBlob) {
+        if (0 <= nos && nos < 5) nos++;
+        else if (0 < nos && nos <= 5) nos--;
+    }
+
+    let tl = gsap.timeline({ defaults: { duration: 1, ease: 'elastic.out(1, 1)', transformOrigin: 'center' } });
+    //Morph blobs.
+    tl
+        .to('#blob0-2', { morphSVG: `#blob${nos}-2` }, 0)
+        .to('#blob0-1', { morphSVG: `#blob${nos}-1` }, 0.15)
+        .to('#blob0-0', { morphSVG: `#blob${nos}-0` }, 0.30)
+    
+    //Translate grouped blob dots.
+    const translateBlob1 = [[0, 0], [-35, -200], [0, -100], [-200, 10], [-50, 100], [-30, -25]]
+    const translateBlob0 = [[0, 0], [50, 250], [0, -50], [200, 0], [100, 0], [75, -30]]
+    tl
+        .to('#group-blob0-1', { xPercent: translateBlob1[nos][0], yPercent: translateBlob1[nos][1] }, 0.2)
+        .to('#group-blob0-0', { xPercent: translateBlob0[nos][0], yPercent: translateBlob0[nos][1] }, 0.25)
+        .to('#group-blob0-1', { duration: 0.4, scale: 1, opacity: 0.5 }, 0.1)
+        .to('#group-blob0-0', { duration: 0.4, scale: 1, opacity: 0.5 }, 0.15)
+        .to('#group-blob0-1', { duration: 0.5, scale: 1, opacity: 1 }, '>')
+        .to('#group-blob0-0', { duration: 0.5, scale: 1, opacity: 1 }, '<0.1');
+    
+    prevBlob = nos;
+}
+
+const blobScene = document.getElementById('blob__scene');
+['click', 'mouseenter', 'mouseleave'].forEach(event =>
+    blobScene.addEventListener(event, changeBlob)
+);
+
+const tl_hello = gsap.timeline({ defaults: { ease: "power3.out" } });
+tl_hello
+    .from("#h > *", { duration: 0.6, stagger: 0.2, drawSVG: "0%" }, 0.25)
+    .from("#e > *", { duration: 0.6, stagger: 0.2, drawSVG: "0%" }, "-=0.7")
+    .from("#l_1 > *", { duration: 0.6, stagger: 0.4, drawSVG: "0%" }, "-=1.2")
+    .from("#l_2 > *", { duration: 0.6, stagger: 0.4, drawSVG: "0%" }, "-=1")
+    .from("#o > *", { duration: 1.2, drawSVG: "0%" }, "-=1.2")
+    .from("#dot > *", { duration: 0.6, scale: 0, transformOrigin: "50% 50%" }, "-=0.8");
+
