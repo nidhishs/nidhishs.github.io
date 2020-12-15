7e471d09aae216147ada5ae9eb80159100bd4e7a
Nidhish <55269918+nidhishs@users.noreply.github.com>
2020-12-15 01:09:34 +0100

    Added menu-btn hover animation. Removed perspective-blob effect.

--- scripts/index.js
+++ scripts/index.js
@@ -5,72 +5,78 @@
 gsap.registerPlugin(MorphSVGPlugin);
 gsap.registerPlugin(DrawSVGPlugin);
 
+const body = document.body;
+window.onload = () => {
+body.classList.remove("loading");
+};
+
+// ============== BLOB ANIMATION ============== //
 // Changes the shape of the blob to one of the 6 predefined shapes.
+let prevBlob = 0;
 function morphBlob() {
-    const min = 0, max = 5;
-    let nos = Math.floor(Math.random() * (max - min + 1) + min);
+  const min = 0,
+    max = 5;
+  let nos = Math.floor(Math.random() * (max - min + 1) + min);
 
-    while (nos == prevBlob) {
-        nos = Math.floor(Math.random() * (max - min + 1) + min);
+  while (nos == prevBlob) {
+    nos = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  let tl = gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: "elastic.out(1, 1)",
+      transformOrigin: "center"
     }
+  });
+  //Morph blobs.
+  tl.to("#blob0-2", { morphSVG: `#blob${nos}-2` }, 0)
+    .to("#blob0-1", { morphSVG: `#blob${nos}-1` }, 0.15)
+    .to("#blob0-0", { morphSVG: `#blob${nos}-0` }, 0.3);
 
-    let tl = gsap.timeline({ defaults: { duration: 1, ease: 'elastic.out(1, 1)', transformOrigin: 'center' } });
-    //Morph blobs.
-    tl
-        .to('#blob0-2', { morphSVG: `#blob${nos}-2` }, 0)
-        .to('#blob0-1', { morphSVG: `#blob${nos}-1` }, 0.15)
-        .to('#blob0-0', { morphSVG: `#blob${nos}-0` }, 0.30);
-    
-    //Translate grouped blob dots.
-    const translateBlob1 = [[0, 0], [-35, -200], [0, -100], [-200, 10], [-50, 100], [-30, -25]]
-    const translateBlob0 = [[0, 0], [50, 250], [0, -50], [200, 0], [100, 0], [75, -30]]
-    tl
-        .to('#group-blob0-1', { xPercent: translateBlob1[nos][0], yPercent: translateBlob1[nos][1] }, 0.2)
-        .to('#group-blob0-0', { xPercent: translateBlob0[nos][0], yPercent: translateBlob0[nos][1] }, 0.25)
-        .to('#group-blob0-1', { duration: 0.4, scale: 1, opacity: 0.5 }, 0.1)
-        .to('#group-blob0-0', { duration: 0.4, scale: 1, opacity: 0.5 }, 0.15)
-        .to('#group-blob0-1', { duration: 0.5, scale: 1, opacity: 1 }, '>')
-        .to('#group-blob0-0', { duration: 0.5, scale: 1, opacity: 1 }, '<0.1');
-    
-    prevBlob = nos;
-}
+  //Translate grouped blob dots.
+  const translateBlob1 = [
+    [0, 0],
+    [-35, -200],
+    [0, -100],
+    [-200, 10],
+    [-50, 100],
+    [-30, -25]
+  ];
+  const translateBlob0 = [
+    [0, 0],
+    [50, 250],
+    [0, -50],
+    [200, 0],
+    [100, 0],
+    [75, -30]
+  ];
+  tl.to(
+    "#group-blob0-1",
+    { xPercent: translateBlob1[nos][0], yPercent: translateBlob1[nos][1] },
+    0.2
+  )
+    .to(
+      "#group-blob0-0",
+      { xPercent: translateBlob0[nos][0], yPercent: translateBlob0[nos][1] },
+      0.25
+    )
+    .to("#group-blob0-1", { duration: 0.4, scale: 1, opacity: 0.5 }, 0.1)
+    .to("#group-blob0-0", { duration: 0.4, scale: 1, opacity: 0.5 }, 0.15)
+    .to("#group-blob0-1", { duration: 0.5, scale: 1, opacity: 1 }, ">")
+    .to("#group-blob0-0", { duration: 0.5, scale: 1, opacity: 1 }, "<0.1");
 
-function perspectiveBlob(e) {
-    const wWidth = window.innerWidth;
-    const wHeight = window.innerHeight;
-
-    // Throttle mousemove, and disable animation on mobile devices.
-    if (!enableCall || wWidth<576) return;
-    enableCall = false;
-
-    let xAxis = ((3 * wWidth) / 4 - e.pageX) / (wWidth/125);
-    let yAxis = (wHeight / 2 - e.pageY) / (wWidth/125);
-
-    blobScene.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-
-    setTimeout(() => (enableCall = true), 120);
+  prevBlob = nos;
 }
 
 const blobScene = document.getElementById('blob__scene');
-const blobContainer = document.querySelector(".land__blob");
-let prevBlob = 0;
-let enableCall = true;
 
-['click', 'mouseenter'].forEach(event =>
-    blobScene.addEventListener(event, morphBlob)
-);
-blobContainer.addEventListener("mousemove", e => perspectiveBlob(e));
+blobScene.addEventListener('mouseenter', throttle(morphBlob, 1000))
+blobScene.addEventListener('click', morphBlob)
+// ============================================ //
 
-blobContainer.addEventListener('mouseenter', () => {
-    blobScene.style.transition = 'transform 130ms linear';
-});
 
-blobContainer.addEventListener('mouseleave', () => {
-    blobScene.style.transition = 'transform 800ms cubic-bezier(0.7, 0, 0.3, 1)';
-    blobScene.style.transform = 'rotateY(0deg) rotateX(0deg)';
-})
-
-//Intro HELLO animation.
+// ============= HELLO ANIMATION ============== //
 const tl_hello = gsap.timeline({ defaults: { ease: "power3.out" } });
 tl_hello
     .from("#h > *", { duration: 0.6, stagger: 0.2, drawSVG: "0%" }, 0.25)
@@ -79,8 +85,10 @@ tl_hello
     .from("#l_2 > *", { duration: 0.6, stagger: 0.4, drawSVG: "0%" }, "-=1")
     .from("#o > *", { duration: 1.2, drawSVG: "0%" }, "-=1.2")
     .from("#dot > *", { duration: 0.6, scale: 0, transformOrigin: "50% 50%" }, "-=0.8");
+// ============================================ //
 
 
+// ============= BOUNCY DOTS ================== //
 // Bouncing dots for brand name and HELLO text
 const hello_text = document.getElementById('hello');
 const hello_dot = document.querySelector('.hello__dot');
@@ -102,4 +110,29 @@ hello_dot.addEventListener('animationend', () => {
 brand_dot.addEventListener('animationend', () => {
     brand_dot.classList.remove('animate');
 });
+// ======================================== //
 
+// ================ MENU-BTN ==============//
+const menuBtn = document.querySelector('.menu__btn');
+const menuOverlay = document.querySelector('.menu__overlay');
+menuBtn.addEventListener("click", () => {
+    body.classList.toggle("menu--open");
+})
+menuOverlay.addEventListener("click", () => {
+    if (body.classList.contains("menu--open"))
+        body.classList.remove("menu--open")
+})
+// ======================================== //
+
+//Re-usable throttle function for event listeners.
+function throttle(callback, interval) {
+  let enableCall = true;
+
+  return function(...args) {
+    if (!enableCall) return;
+
+    enableCall = false;
+    callback.apply(this, args);
+    setTimeout(() => enableCall = true, interval);
+  }
+}
