1577d0ff6febf2251cf1e046e422ee91b0b891ce
Nidhish <nidhish@nidhishs-mbp.local>
2022-04-24 22:58:42 +0200

    feat: Initial version of portfolio

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const projects = [
+    {
+        name: 'AI Shooter',
+        desc: 'A Unity based shooting game where the Blue agent learns to shoot all the red targets using reinforcement learning.',
+        image: './assets/images/ai_shooter_project.png',
+        src: 'https://github.com/nidhishs'
+    },
+    {
+        name: 'Sentiment Analysis',
+        desc: 'A text-classification (CNN-BiLSTM) model to measure brand perception on social media platforms like Twitter with upto 82% accuracy.',
+        image: './assets/images/sentavis_project.gif',
+        src: 'https://github.com/nidhishs/sentiment-analysis'
+    },
+]
+
+const articles = [
+    {
+        name: '5 SCSS Mixins to check out!',
+        desc: "I talk about what mixins are, why you should use them, and share 5 of my favourite mixins that I\'ve written."
+    }
+]
+
+Vue.createApp({
+    data() {
+        return {
+            projects: projects,
+            articles: articles
+        }
+    }
+}).mount('#content-wrapper')
